d5b5af7c665db32ece5a9aa009a8f474305b2189
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2022-08-17 00:01:05 -0300

    Ejercicio 2.9 cosot camion con csv listo

--- Clase02/costo_camion.py
+++ Clase02/costo_camion.py
@@ -3,19 +3,21 @@
 """
 @author: mbalderr
 """
+import csv
+
 def costo_camion(nombre_archivo):
     precio_total = 0
     precio_cajon = 0
     cant_cajones = 0
-
-    with open(nombre_archivo, 'rt') as f:
-        next(f)
-        for line in f:            
-            row = line.split(',') 
-            precio_cajon = (float(row[2].strip('\n'))) 
-            cant_cajones = (float(row[1])) 
-            precio_total = precio_total + (precio_cajon * cant_cajones)
-        
+    f = open(nombre_archivo)
+    rows = csv.reader(f)
+    headers = next(rows)        
+    for row in rows:            
+       
+        precio_cajon = (float(row[2].strip('\n'))) 
+        cant_cajones = (float(row[1])) 
+        precio_total = precio_total + (precio_cajon * cant_cajones)
+    f.close()    
     return precio_total
 
 costo = costo_camion('../Data/camion.csv')
